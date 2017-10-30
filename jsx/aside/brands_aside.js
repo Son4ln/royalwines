516f1e50e18c3e859339cd7c7a0cbdc520ff12cc
Tobias Nguyen <git@3spayroll.com>
2017-10-30 17:44:23 +0700

    complete cart

--- jsx/aside/brands_aside.js
+++ jsx/aside/brands_aside.js
@@ -7,16 +7,24 @@ class BrandsAside extends React.Component {
     super();
   }
 
+  componentWillMount() {
+    const script = document.createElement('script');
+    script.src = '/public/assets/site/js/main.js';
+    script.async = true;
+
+    document.body.appendChild(script);
+  }
+
   render() {
   
     return(
-      <section className="ct-sidebar ct-js-sidebar ct-js-background ct-u-displayTable" data-bg="/public/assets/site/images/content/demo2.jpg">
-        <div className="ct-sidebar-inner rw">
+      <section className="ct-sidebar ct-js-sidebar ct-js-background ct-u-displayTable ct-u-hideAnimateBg" data-bg="/public/assets/site/images/content/demo2.jpg">
+        <div className="ct-sidebar-inner">
           <div className="item">
             <hr className="hr-custom ct-js-background animated" data-fx="fadeInDown" data-bg="/public/assets/site/images/hr2.png" data-bgrepeat="no-repeat" />
-            <h2 className="ct-u-font2 text-uppercase animated" data-fx="flipInY">giỏ hàng</h2>
-            <p className="animated" data-fx="fadeIn">Đây là cái mô tả</p>
-            <hr className="hr-custom ct-js-background ct-u-paddingTop15 animated" data-fx="fadeInDown" data-bg="/public/assets/site/images/hr2.png" data-bgrepeat="no-repeat" />
+            <h2 className="ct-u-font2 text-uppercase animated " data-fx="flipInY">nhãn hiệu</h2>
+            <p className="animated" data-fx="fadeIn">Etiam vitae tempus lorem. Interdum et malesuada</p>
+            <hr className="hr-custom ct-js-background ct-u-paddingTop15 animated ct-u-paddingTop40" data-fx="fadeInDown" data-bg="/public/assets/site/images/hr2.png" data-bgrepeat="no-repeat" />
           </div>
         </div>
       </section>
