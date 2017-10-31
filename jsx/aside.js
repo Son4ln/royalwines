fd8234becc558c98925f20821337125ddc5ab7fa
Tobias Nguyen <git@3spayroll.com>
2017-10-31 16:45:26 +0700

    checkout page

--- jsx/aside.js
+++ jsx/aside.js
@@ -5,6 +5,7 @@ import { Route, Switch } from 'react-router-dom';
 import HomeAside from './aside/home_aside';
 import CartAside from './aside/cart_aside';
 import WishlistAside from './aside/wishlist_aside';
+import CheckoutAside from './aside/checkout_aside';
 import BrandsAside from './aside/brands_aside';
 import ProductsAside from './aside/products_aside';
 import LoginAside from './aside/login_aside';
@@ -23,7 +24,8 @@ class Aside extends React.Component {
         <Switch>
           <Route exact path='/' render={() => <HomeAside />}/>
           <Route exact path='/gio-hang' render={() => <CartAside />}/>
-          <Route exact path='/yeu-thich' render={() => <WishlistAside />}/> 
+          <Route exact path='/yeu-thich' render={() => <WishlistAside />}/>
+          <Route exact path='/thanh-toan' render={() => <CheckoutAside />}/>
           <Route exact path='/nhan-hieu' render={() => <BrandsAside />}/>
           <Route exact path='/san-pham' render={() => <ProductsAside />}/>
           <Route exact path='/dang-nhap' render={() => <LoginAside />}/>
