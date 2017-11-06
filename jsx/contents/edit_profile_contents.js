6d4a48a16c369cf7ce89be78e2dd713cf00a4c96
son4ln <son4ln@gmail.com>
2017-11-06 19:48:27 +0700

    profile

--- jsx/contents/edit_profile_contents.js
+++ jsx/contents/edit_profile_contents.js
@@ -1,26 +1,125 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { renderMainScript } from '../utils';
+import {connect} from 'react-redux';
+import * as actions from '../store/actions';
 
 class EditProfileContents extends React.Component {
   constructor() {
     super();
+    this.state = {
+      user: {}
+    }
+
+    this.saveNewUser = this.saveNewUser.bind(this);
+  }
+
+  componentWillMount() {
+    axios.get('/site/controller/controller.php?action=checkLogin')
+    .then(res => this.checkUser(res.data));
   }
 
   componentDidMount() {
     let scriptBlock = document.getElementById('edit-script-blog');
     renderMainScript(scriptBlock);
+
+    $('#update-user').submit((e) => {
+      this.updateUser(e);
+    });
+  }
+
+  checkUser(data) {
+    if (data === 'not_login') {
+      window.location.href = '/';
+      return;
+    }
+
+    this.setState({
+      user: data
+    });
+
+    $('#name').val(data.full_name);
+    $('#phone').val(data.phone);
+    $('#address').val(data.address);
+  }
+
+  updateUser(e) {
+    e.preventDefault();
+
+    if ($('#name').val() === '') {
+      $('#alert-update').html('Vui lòng nhập họ và tên');
+      $('#alert-update').removeClass('hidden');
+      return;
+    }
+
+    if ($('#phone').val() === '') {
+      $('#alert-update').html('Vui lòng nhập số điện thoại');
+      $('#alert-update').removeClass('hidden');
+      return;
+    }
+
+    if ($('#address').val() === '') {
+      $('#alert-update').html('Vui lòng nhập địa chỉ');
+      $('#alert-update').removeClass('hidden');
+      return;
+    }
+
+    $('#submit-update').val('Đang cập nhập...');
+
+    axios.post('/site/controller/controller.php?action=updateUser', {
+      name: $('#name').val(),
+      addr: $('#address').val(),
+      phone: $('#phone').val(),
+      pass: $('#pass').val(),
+      retype: $('#pass-retype').val()
+    })
+    .then(res => {
+      if (res.data === 'success') {
+        $('#alert-update').addClass('hidden');
+        $('#submit-update').val('Cập nhập thông tin');
+        this.saveNewUser();
+      } else if (res.data === 'retype_wrong') {
+        $('#alert-update').html('Nhập lại mật khẩu sai');
+        $('#alert-update').removeClass('hidden');
+      }
+    });
+  }
+
+  saveNewUser() {
+    axios.get('/site/controller/controller.php?action=checkLogin')
+    .then(res => {
+      let saveUser = this.props.onSaveUser;
+      saveUser(res.data);
+    });
   }
 
   render() {
+    let alert_style = {
+      color: 'red'
+    }
+
     return(
       <section className="ct-content ct-full-height ct-u-displayTable">
         <div className="ct-register animated" data-fx="pulse">
-          <form role="form" action="assets/form/send.php" method="post" className="contactForm validateIt ct-u-paddingTop10" data-email-subject="Contact Form" data-show-errors="true">
+          <center>
+            <h4 className="ct-u-font2 text-uppercase
+            ct-u-marginTop10 animated hidden" id="alert-update" data-fx="flipInY" style={alert_style}>
+
+            </h4>
+          </center>
+          <form id="update-user" role="form" className="contactForm validateIt ct-u-paddingTop10" data-email-subject="Contact Form" data-show-errors="true">
             <div className="row">
               <div className="col-md-12">
                 <div className="form-group">
                   <input id="name" placeholder="Họ và tên" required type="text" className="form-control input-lg"/>
+                </div>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-12">
+                <div className="form-group">
+                  <input id="email" readonly value={this.state.user.email} placeholder="Email" required type="text" className="form-control input-lg"/>
                 </div>
               </div>
             </div>
@@ -44,7 +143,7 @@ class EditProfileContents extends React.Component {
             <div className="row">
               <div className="col-md-12">
                 <div className="form-group">
-                  <input id="pass" placeholder="Mật khẩu" required type="password" className="form-control input-lg"/>
+                  <input id="pass" placeholder="Mật khẩu" type="password" className="form-control input-lg"/>
                 </div>
               </div>
             </div>
@@ -52,7 +151,7 @@ class EditProfileContents extends React.Component {
             <div className="row">
               <div className="col-md-12">
                 <div className="form-group">
-                  <input id="pass-retype" placeholder="Nhập lại mật khẩu" required type="password" className="form-control input-lg"/>
+                  <input id="pass-retype" placeholder="Nhập lại mật khẩu" type="password" className="form-control input-lg"/>
                 </div>
               </div>
             </div>
@@ -60,7 +159,7 @@ class EditProfileContents extends React.Component {
             <div className="row">
               <div className="col-xs-12">
                 <div className="form-group">
-                  <input type="submit" className="btn btn-lg btn-block btn-button--dark" value="cập nhật thông tin"/>
+                  <input type="submit" id="submit-update" className="btn btn-lg btn-block btn-button--dark" value="cập nhật thông tin"/>
                 </div>
               </div>
             </div>
@@ -73,4 +172,14 @@ class EditProfileContents extends React.Component {
   }
 }
 
-export default EditProfileContents
+const mapStateToProps = (state) => {
+  return {}
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSaveUser: (user) => {dispatch(actions.save_user(user))}
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditProfileContents);
